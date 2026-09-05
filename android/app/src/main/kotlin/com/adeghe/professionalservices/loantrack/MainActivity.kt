5d559c2cc2f51337d68771d29e8187d2416e5f4f
eghosa001 <aighewieghosa111@gmail.com>
2026-09-05 07:18:22 +0100

    fix: move Android activity to branded package

---
+++ android/app/src/main/kotlin/com/adeghe/professionalservices/loantrack/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.adeghe.professionalservices.loantrack
+
+import android.os.Bundle
+import android.view.WindowManager
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity : FlutterFragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Prevent screenshots, screen recording, and app-switcher previews of
+        // financial customer data (BVN/NIN, loan details).
+        window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
+    }
+}
